ca8541202ac8c9e28bc2418842f491d7410594c9
LePhuocLoc0503 <lephuocloc0503@gmail.com>
2023-03-29 04:09:57 +0700

    lan 3

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
-    #path('detail/<int:id>', views.detail(), name="detail"),
-    path('list/', views.viewlist, name="view_list"),
+    path('list/', views.viewlist, name="list"),
+    path('detail/<int:questions_id>', views.detail, name="details"),
+    path('<int:questions_id>',views.vote,name="vote")
 ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
-from .models import Question , Questions
+from .models import Question, Questions
 from django.http import HttpResponse
+
+
 # Create your views here.
 def index(request):
-    myname ="abcabcabc"
-    abc =["abcb","masi","asdsa"]
-    context = { "name": myname ,"text":abc }
-    return render(request, "polls/index.html", context )
+    myname = "abcabcabc"
+    abc = ["abcb", "masi", "asdsa"]
+    context = {"name": myname, "text": abc}
+    return render(request, "polls/index.html", context)
+
 
 def viewlist(request):
-    list_question= Question.objects.all()
+    list_question = Questions.objects.all()
     context = {"dsquest": list_question}
-    return render(request,"polls/question_list.html",context)
-
-def detail(request,questions_id):
-    ids = Questions.objects.get(pk=questions_id)
-    return render(request, "polls/detail.html", {"a": ids})
+    return render(request, "polls/question_list.html", context)
 
 
+def detail(request, questions_id):
+    q = Questions.objects.get(pk=questions_id)
+    return render(request, "polls/detail.html", {"q": q})
 
+def vote(request,questions_id):
+    q = Questions.objects.get(pk=questions_id)
+    try:
+        dlieu = request.POST["choice"]
+        c = q.choices_set.get(pk=dlieu)
+    except:
+        HttpResponse("lỗi")
+    c.votes = c.votes + 1
+    c.save()
+    return render(request,"polls/result.html",{"q":q})
+
+#
+        #
